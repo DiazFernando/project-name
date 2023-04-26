7f48a833e776645af96fa2d67f4e0435b9c97e7e
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-04-26 20:45:26 -0300

    clase sergio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonaController } from './persona/persona.controller';
 import { PersonaService } from './persona/persona.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({rootPath:join(__dirname,'..','client')})],
   controllers: [AppController, PersonaController],
   providers: [AppService, PersonaService],
 })
